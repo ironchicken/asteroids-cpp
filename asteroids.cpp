540a7f516c575ccb86ad138ff85f44403f15b939
Richard Lewis <richard@rjlewis.me.uk>
2022-12-04 22:42:48 +0000

    Implement main function

--- asteroids.cpp
+++ asteroids.cpp
@@ -1,5 +1,13 @@
 #include "Game.hpp"
 
 int main(int argc, char* argv[]) {
-    return 0;
+    Game game;
+
+    auto initialized = game.initialize();
+    if (initialized) {
+        game.run();
+    }
+    game.shutDown();
+
+    return initialized ? 0 : 1;
 }
